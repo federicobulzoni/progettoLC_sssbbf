cdd50a26ad9b0b763d4b62580a0530ff43c22b1f
Simone Scaboro <scaboro.simone@gmail.com>
2020-06-18 13:33:24 +0200

    Corretto PrintTAC

--- scala40Compiler/test/testino.scala
+++ scala40Compiler/test/testino.scala
@@ -1,3 +1,6 @@
+def prova(a : Int)(b : Float, c : *Char) : Float = a + b;
+def prova2(a : Int)(b : Float, c : *Char) = writeInt(3);
+
 def main () = {
 	var c : Int = 3;
 	var b : Array[*Int](3);
@@ -5,4 +8,8 @@ def main () = {
 
 	*b[1] = 3;
 	a[1] = b;
+	var t : Int;
+	var g : Char;
+	var k : Float = prova(c)(t, &g);
+	prova2(c)(t, &g);
 }
