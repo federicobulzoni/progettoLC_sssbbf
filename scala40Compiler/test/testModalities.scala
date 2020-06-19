9618a7ba0b7486bbe7f7fdbfe82c1c6701ab6371
Simone Scaboro <scaboro.simone@gmail.com>
2020-06-19 21:35:25 +0200

    Aggiunto controllo passaggio valori (Sicuramente da controllare e ottimizzare)

--- scala40Compiler/test/testModalities.scala
+++ scala40Compiler/test/testModalities.scala
@@ -1,16 +1,17 @@
 
 def main() = {
-	def foo(ref a : Int)(val b : Int)(res c : Int)(valres d : Int) = {
+	def foo(ref a : Int)(val b : Int)(res c : Int)(valres d : Int) : Int = {
 		a = a + 1;
 		d = a;
 		c = 2;
 		var r : Int = a;
 		var p : Int = a + b;
+		return 2;
 	}
 	var x : Int = 1;
 	var y : Int = 2;
 	var w : Int = 5;
-	foo(1)(1)(x)(y);
+	var l : Float = foo(w)(1)(x)(y);
 	var z : **Int;
 	**z = 4;
 
