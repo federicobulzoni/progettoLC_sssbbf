b37ff29b7caa6ed8ecadb5d902471fd6b606c5fa
Sara <sarabia1206@gmail.com>
2020-06-20 11:20:46 +0200

    Modificato errore parametri e spostati test definitivi.

--- scala40Compiler/test/corretti/modalities.scala
+++ scala40Compiler/test/corretti/modalities.scala
@@ -1,4 +1,4 @@
-
+// Meaningless example using all four parameter modes.
 def main() = {
 	def foo(ref a : Int)(val b : Int)(res c : Int)(valres d : Int) : Int = {
 		a = a + 1;
@@ -11,9 +11,7 @@ def main() = {
 	var x : Int = 1;
 	var y : Int = 2;
 	var w : Int = 5;
-	var l : Float = foo(1)(1)(&x)(y);
-	var z : **Int;
-	**z = 4;
+	var l : Float = foo(x)(1)(y)(w);
 
 	def bar(ref a : Array[Int](2)) = {
 		a[1] = 4;
