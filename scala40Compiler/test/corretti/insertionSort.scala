c01aabe0d7fe585eae53776bdb5bfbddafd16d66
Sara <sarabia1206@gmail.com>
2020-06-20 17:04:45 +0200

    Modificato insertion sort.

--- scala40Compiler/test/corretti/insertionSort.scala
+++ scala40Compiler/test/corretti/insertionSort.scala
@@ -1,18 +1,19 @@
 // insertion sort
-var N : Int = 100;
+var N : Int = 10;
 
 def insertion_sort(ref A : Array[Int](10)) = {
-    var j : Int = 2;
+    var j : Int = 1;
     var key : Int;
     var i : Int;
     while(j < N) {
         var key : Int = A[j];
         i = j - 1;
-        while(i > 0 && A[i] > key){
+        while(i >= 0 && A[i] > key){
             A[i + 1] = A[i];
             i -= 1;
         }
         A[i + 1] = key;
+        j += 1;
     }
 }
 
